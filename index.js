80e3c02fa27b1cb616f1db0fa158e2239ef4427a
amaar <amaar.ahmednur@student.moringaschool.com>
2023-09-19 12:03:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Welcome to Flatbook';
+const welcomeMessage = 'Welcome to Flatbook, currentUse!';
+const excitedWelcomeMessage ="WELCOME TO FLATBOOK, currentUse!";
+const shortGreeting = "Welcome, Amar!";
